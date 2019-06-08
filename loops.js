9819b55f7e0e1c971ca26d321e55718021b0d11e
David Little <littledavid175@gmail.com>
2019-06-08 17:07:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,6 +16,15 @@ function whileLoop (n) {
 }
   
 function doWhileLoop(num) {
-  
+  var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("I run once regardless");
+} while (incrementVariable() < 5);
   
 }
